0d6e8d7e8cebd1eafa870d6d6d51ffd43267042f
liaogx <liaogx666@gmail.com>
2021-01-03 17:34:17 +0800

    感染数据按日期降序排列

--- coronavirus/models.py
+++ coronavirus/models.py
@@ -41,7 +41,7 @@ class Data(Base):
     created_at = Column(DateTime, server_default=func.now(), comment='创建时间')
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), comment='更新时间')
 
-    __mapper_args__ = {"order_by": confirmed}
+    __mapper_args__ = {"order_by": date.desc()}  # 按日期降序排列
 
     def __repr__(self):
         return f'{repr(self.date)}：确诊{self.confirmed}例'
